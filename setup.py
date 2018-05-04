a1d84d2ee51bc7edaad072f83d25cc7cf7e8473c
ProjetISNLaxe <35565165+ProjetISNLaxe@users.noreply.github.com>
2018-05-04 14:41:31 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup, Executable
-setup(
-    name="Retravel",
-    version="1.0",
-    description="Retravel",
-    executables=[Executable(script="launcher.py", icon="icone.ico", base="Win32GUI",)])
